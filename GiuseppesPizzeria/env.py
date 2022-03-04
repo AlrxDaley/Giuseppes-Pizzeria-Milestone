24182a79f921d79b7a6362982debfd14c8d3cd94
AlrxDaley <alexander_daley@icloud.com>
2022-03-04 11:43:24 +0000

    integrated postgres and deployed app to heroku

---
+++ GiuseppesPizzeria/env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ['DATABASE_URL'] = "postgres://boltdqjsxeisod:5195e8113d556a8f9a4de3dc39969dee6dc4f4133221ec16b2736bec76bf781f@ec2-52-31-201-170.eu-west-1.compute.amazonaws.com:5432/dej7vvbqa8bgug"
+os.environ['SECRET_KEY'] = 'Secretkey2001'
