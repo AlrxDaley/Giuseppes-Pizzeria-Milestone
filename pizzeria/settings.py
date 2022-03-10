bbbcaa052388ccd54f46fe54fbd3c2e3f884d02c
AlrxDaley <alexander_daley@icloud.com>
2022-03-10 09:21:14 +0000

    added base.html file , added booking class to models.py and installed and added django phone field to installed apps

--- pizzeria/settings.py
+++ pizzeria/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'django_summernote',
     'tableapp',
+    'phone_field',
     
 ]
 
--- tableapp/models.py
+++ tableapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from phone_field import PhoneField
 
 # Create your models here.
+
+class booking(models.Model, PhoneField):
+    booking_date = models.DateTimeField(auto_now=True)
+    number_of_guests = models.IntegerField(default=0)
+    first_name = models.CharField(max_length=50, min_length = 1)
+    last_name = models.CharField(max_length=50, min_length = 1)
+    phone_number = models.PhoneField(blank=True, help_text='Contact phone number')
+    special_request = models.TextField(blank=True)
+    
+    class Meta:
+        ordering = ['-created_on']
+        
+    def __str__(self):
+        return self.first_name
